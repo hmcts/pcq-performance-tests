4a141b8b695b6d2e4d1423bb6ebfc017072e5118
Dave Jones <dave.jones@hmcts.net>
2023-02-05 12:29:42 +0000

    PCQ-1639: Testing perf test.

--- src/gatling/simulations/uk/gov/hmcts/pcq/scenarios/checks/CsrfCheck.scala
+++ src/gatling/simulations/uk/gov/hmcts/pcq/scenarios/checks/CsrfCheck.scala
@@ -5,18 +5,18 @@ import io.gatling.core.check.CheckBuilder
 import io.gatling.http.Predef._
 import io.gatling.http.check.HttpCheck
 import jodd.lagarto.dom.NodeSelector
-import io.gatling.core.check.extractor.css.CssCheckType
+import io.gatling.core.check.css.CssCheckType
 
 
 object CsrfCheck {
-  def save: CheckBuilder[CssCheckType, NodeSelector, String] = css("input[name='_csrf']", "value").saveAs("csrf")
+  def save: CheckBuilder[CssCheckType, NodeSelector] = css("input[name='_csrf']", "value").saveAs("csrf")
 
   def csrfParameter: String = "_csrf"
   def csrfTemplate: String = "${csrf}"
 }
 
 object CsrfCheckForPayment {
-  def save: CheckBuilder[CssCheckType, NodeSelector, String] = css("input[name='csrfToken']", "value").saveAs("csrfToken")
+  def save: CheckBuilder[CssCheckType, NodeSelector] = css("input[name='csrfToken']", "value").optional.saveAs("csrfToken")
 
   def csrfParameterForPayment: String = "csrfToken"
   def csrfTemplateForPayment: String = "${csrfToken}"
--- src/gatling/simulations/uk/gov/hmcts/pcq/scenarios/checks/CurrentPageUrl.scala
+++ src/gatling/simulations/uk/gov/hmcts/pcq/scenarios/checks/CurrentPageUrl.scala
@@ -6,10 +6,10 @@ import io.gatling.http.Predef._
 import io.gatling.http.check.HttpCheck
 import jodd.lagarto.dom.NodeSelector
 import io.gatling.http.check.url.CurrentLocationCheckType
-import io.gatling.core.check.extractor.css.CssCheckType
+import io.gatling.core.check.css.CssCheckType
 
 object CurrentPageUrl {
-  def save: CheckBuilder[CurrentLocationCheckType,String,String] = currentLocation.saveAs("currentPageUrl")
+  def save: CheckBuilder[CurrentLocationCheckType,String] = currentLocation.saveAs("currentPageUrl")
 
   def currentPageTemplate: String = "${currentPageUrl}"
 }
